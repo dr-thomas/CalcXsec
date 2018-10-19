0086d71d18a8b325de580553eea383757793cc13
Thomas Campbell <thomascampbell1@gmail.com>
2018-10-19 12:22:09 -0600

    setting branches the right way

---
+++ CalcXsec.C
@@ -0,0 +1,45 @@
+#include <TString.h>
+#include <TFile.h>
+#include <TMatrixD.h>
+#include <TTree.h>
+#include "./util/p0dCCEventClass.C"
+
+void CalcXsec(){
+	//define files
+	TString inFMCStr = "/Users/thomascampbell/p0dCCAnalysis/FitResults/Macros/XsecDrawFiles/WaterSubFullNom.root";
+	TString inFMCGenieStr = "/Users/thomascampbell/p0dCCAnalysis/FitResults/Macros/XsecDrawFiles/GenWithFlagGENIE.root";
+	TString inFMCEffStr = "/Users/thomascampbell/p0dCCAnalysis/FitResults/Macros/XsecDrawFiles/GenWithFlag.root";
+	TString inFFitStr = "/Users/thomascampbell/p0dCCAnalysis/FitResults/DataFits/NEUT/fitBaseOutNoReg.root";
+	TString inFDataStr = "/Users/thomascampbell/p0dCCAnalysis/FitResults/Macros/XsecDrawFiles/WaterSubData.root";
+
+	TFile* inFMC = new TFile(inFMCStr,"OPEN");
+	TFile* inFMCEff = new TFile(inFMCEffStr,"OPEN");
+	TFile* inFMCGenie = new TFile(inFMCGenieStr,"OPEN");
+	TFile* inFFit = new TFile(inFFitStr,"OPEN");
+	TFile* inFData = new TFile(inFDataStr,"OPEN");
+
+	//define trees
+	TTree* truthT = (TTree*)inFMCEff->Get("outTtruth");
+	p0dCCEvent* genEvt = new p0dCCEvent();
+	genEvt->SetBranchAddresses(truthT, true, false);
+	int nEntriesTruth = truthT->GetEntries();
+
+	TTree* defaultT = (TTree*)inFMCEff->Get("outTdefault");
+	p0dCCEvent* selEvt = new p0dCCEvent();
+	selEvt->SetBranchAddresses(defaultT, false, false);
+	int nEntriesD = defaultT->GetEntries();
+
+	//Genie
+	//TODO: add weight to Genie stuff, re-produce those comparisions...
+	TTree* truthTG = (TTree*)inFMCGenie->Get("outTtruth");
+	p0dCCEvent* genEvtGENIE = new p0dCCEvent();
+	genEvtGENIE->SetBranchAddresses(truthTG, true, true);
+	int nEntriesTruthG = truthTG->GetEntries();
+
+
+	//set up cov matrices 
+	//intilize results (ngen, nsel, etc) 
+	//loop over events, loop over toys
+	//calc xsec
+	//draw results
+}
---
+++ util/p0dCCEventClass.C
@@ -0,0 +1,51 @@
+#include <TMatrixD.h>
+#include <TTree.h>
+
+class p0dCCEvent {
+	public:
+		TMatrixD* WeightsMatrix;//15,400
+		Int_t topology;
+		Int_t nu_pdg;
+		Float_t nu_trueE;
+		Float_t truelepton_mom;
+		Float_t truelepton_costheta;
+		Float_t* vtx_truepos;//4
+		Int_t IsOnWater;
+		Float_t weightHL;
+
+		p0dCCEvent() {
+			TMatrixD WeightsMatrixInit(15,400);
+			WeightsMatrix = &WeightsMatrixInit;
+			topology=-1;
+			nu_pdg=-1;
+			nu_trueE=-1;
+			nu_trueE=-999.;
+			truelepton_mom=-999.;
+			truelepton_costheta=-999.;
+			vtx_truepos = new Float_t[4];
+			IsOnWater=-1;
+			weightHL=-1;
+		}
+		~p0dCCEvent() {
+			delete []vtx_truepos;
+			//TODO how to delete: delete WeightsMatrixInit;
+		}
+
+		void SetBranchAddresses(TTree* inT, bool isMC, bool isGENIE) {
+			if(!isGENIE){
+				inT->SetBranchAddress("WeightsMatrix",&WeightsMatrix);
+				inT->SetBranchAddress("weight",&weightHL);
+			}
+			inT->SetBranchAddress("topology",&topology);
+			inT->SetBranchAddress("nu_pdg",&nu_pdg);
+			inT->SetBranchAddress("nu_trueE",&nu_trueE);
+			inT->SetBranchAddress("truelepton_mom",&truelepton_mom);
+			inT->SetBranchAddress("truelepton_costheta",&truelepton_costheta);
+			inT->SetBranchAddress("IsOnWater",&IsOnWater);
+			if(isMC){
+				inT->SetBranchAddress("truevtx_pos",vtx_truepos);
+			} else {
+				inT->SetBranchAddress("selvtx_truepos",vtx_truepos);
+			}
+		}
+};
